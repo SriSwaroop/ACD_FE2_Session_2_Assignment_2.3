680108dfb2ac0702a58bfee1d1015b4c24a9a08e
SrinivasErram <srinivas.erram13@gmail.com>
2017-01-25 08:57:57 +0530

    Add files via upload

---
+++ Promises.js
@@ -0,0 +1,44 @@
+
+
+
+function display(boolean){
+
+// document.getElementById('lblStatusstart').innerHTML = 'Started';	
+chnglbl('lblStatusstart','Started');
+
+var promobj = new Promise(function(resolve,reject){
+
+chnglbl('lblPromisestart','Promise Started');
+
+var iscompleted = boolean;
+
+if(iscompleted){
+
+chnglbl('lblprommade',' Promise is Made');
+
+ resolve('Resolved');
+	} else{
+
+		 chnglbl('lblprommade',' Promise is Made');
+
+ 	reject('Rejected');
+ }
+
+});
+
+promobj.then(function(resolvedstatus){
+
+chnglbl('lblpromfulfiled',' Promise is fulfiled and Status is '+resolvedstatus);
+
+}).catch(function(rejectstatus){
+
+chnglbl('lblpromfulfiled',' Promise is fulfiled and Status is '+rejectstatus);
+
+	});
+}
+
+function chnglbl(id,msg){
+
+	document.getElementById(id).innerHTML = msg;
+
+}
